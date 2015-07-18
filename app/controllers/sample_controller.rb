852dd9e6158dd5585055ffeadb53588b1995ba7a
Shuhei Hiya <syuhei176@gmail.com>
2015-07-19 06:39:03 +0900

    Updated

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -6,7 +6,7 @@ class SampleController < ApplicationController
   	if params.key?(:name) || params.key?(:pass)
   		exp = Time.now.to_i + 4 * 3600
   		payload = {:name => params[:name], :exp => exp }
-  		hmac_secret = '643fe2b25f782ac50a94a93887026575364e965afb0b0683047fc0ecd278b9809a4516f301bfa6b99b4ef325bd363511'
+  		hmac_secret = '{milkcocoa-secret}'
   		token = JWT.encode payload, hmac_secret, 'HS256'
   		@msg = 'Logged in as ' + params[:name]
   		@token = token
